f6a6e314c9b463b4e5013feb1899fc5cc4f52676
hanzala1234 <42064189+hanzala1234@users.noreply.github.com>
2021-11-30 14:27:27 +0500

    Update title of UI

--- src/app/review/reviews.tsx
+++ src/app/review/reviews.tsx
@@ -10,7 +10,7 @@ export const Reviews = () => {
   return (
     <Container maxWidth="lg">
       <Box paddingTop={10}>
-        <Typography color="primary" variant="h4">Review UI</Typography>
+        <Typography color="primary" variant="h4">Review BOL</Typography>
       </Box>
 
       <Box paddingY={10}>
